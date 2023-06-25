33f3c3cd504c3e42296d9b9440504ab8e385a587
Paulo André <pauloandrejanga@gmail.com>
2023-06-25 19:35:36 -0300

    Inserindo script para animar palavra tela inicial

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
+//Adiciona responsividade do menu
 let menuAberto = false;
 let botao_menu = document.getElementById("botao_menu");
-botao_menu.onclick = controlarMenu;
+botao_menu.onclick = addMobile;
 
-function controlarMenu() {
+function addMobile() {
   if (menuAberto === false) {
     menuAberto = true;
     document.getElementById("normal").style.display = "flex";
@@ -12,10 +13,10 @@ function controlarMenu() {
   }
 }
 
-
+//Retira versão mobile do menu
 let larguraAnterior = window.innerWidth;
-window.addEventListener("resize", ajustarEstilo);
-function ajustarEstilo() {
+window.addEventListener("resize", removerMobile);
+function removerMobile() {
   const larguraAtual = window.innerWidth;
   if (larguraAtual > 768) {
     document.getElementById("normal").style.display = "flex";
@@ -25,3 +26,34 @@ function ajustarEstilo() {
 
   larguraAnterior = larguraAtual;
 }
+
+
+//Palavra da tela inicial
+const palavras = ["conceito", "sabor", "estilo"];
+const tempoDigitacao = 210;
+let palavraAtual = 0;
+let caractereAtual = 0;
+let animacaoSpan = document.getElementById("animacaoSpan");
+
+function animarPalavra() {
+  if (caractereAtual < palavras[palavraAtual].length) {
+    animacaoSpan.textContent += palavras[palavraAtual].charAt(caractereAtual);
+    caractereAtual++;
+    setTimeout(animarPalavra, tempoDigitacao);
+  } else {
+    setTimeout(apagarPalavra, tempoDigitacao * 2);
+  }
+}
+
+function apagarPalavra() {
+  if (caractereAtual >= 0) {
+    animacaoSpan.textContent = animacaoSpan.textContent.slice(0, -1);
+    caractereAtual--;
+    setTimeout(apagarPalavra, tempoDigitacao);
+  } else {
+    palavraAtual = (palavraAtual + 1) % palavras.length;
+    setTimeout(animarPalavra, tempoDigitacao);
+  }
+}
+
+animarPalavra();
